3714cd1e4ad708e963c16fb6d6168da666f42611
Zach Eggert <egger0a6@email.arizona.edu>
2022-07-07 22:58:38 -0400

    add more styling to StarshipPage, add async function to fetch a starship's pilots and added them to starship details inside of StarshipPage component

--- src/pages/StarshipPage/StarshipPage.jsx
+++ src/pages/StarshipPage/StarshipPage.jsx
@@ -1,14 +1,43 @@
+import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { getPilots } from "../../services/sw-api";
 
 const StarshipPage = () => {
   const location = useLocation()
+  const [pilots, setPilots] = useState([])
+
+  useEffect(() => {
+    const fetchPilots = async () => {
+      const pilots = await getPilots(location.state.starship.pilots);
+      setPilots(pilots);
+    }
+    fetchPilots();
+  }, [location.state.starship.pilots])
 
   return (
     <div className="details-container">
       <div className="details-card">
-        <p>{location.state.starship.name}</p>
-        <p>{location.state.starship.model}</p>
-        <Link to="/">RETURN</Link>
+        <h2>{location.state.starship.name}</h2>
+        <p>
+          MODEL: <span style={{fontStyle:"italic"}}>
+          {location.state.starship.model}
+          </span>
+        </p>
+        {pilots?.length ?
+          <>
+            Pilots:
+            <ul aria-label="Pilots:">
+              {pilots.map(pilot => 
+                <li key={pilot.name}>{pilot.name}</li>
+              )}
+            </ul>
+          </>
+          :
+          <>
+            <p>No Pilots</p>
+          </>
+        }
+        <Link to="/" className="return-btn">RETURN</Link>
       </div>
     </div>
   );
